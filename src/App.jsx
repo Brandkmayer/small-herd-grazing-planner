52b00ac4101a1b00f30e8a6299b595e364a5e6c7
Brandon Mayer <butterzs@arizona.edu>
2025-11-13 11:33:25 -0700

    Changed the name of perennial ADA column to Introduced

--- src/App.jsx
+++ src/App.jsx
@@ -830,7 +830,7 @@ export default function GrazingPlanner() {
                         <th className="p-2 w-40">Herd Size</th>
                         <th className="p-2">Prev. Planned ADA</th>
                         <th className="p-2">Prev. Actual ADA</th>
-                        <th className="p-2">Est. Perennial ADA (this yr)</th>                        
+                        <th className="p-2">Est. Introduced ADA (this yr)</th>                        
                         <th className="p-2">Est. Native ADA (this yr)</th>
                         <th className="p-2">Projected Grazing Days</th>
                         <th className="p-2">Proposed ADA (this yr)</th>
